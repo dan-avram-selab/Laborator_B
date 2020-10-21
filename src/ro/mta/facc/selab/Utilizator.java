72f1e8da8992411a7b83f50e9e6d611cca30f785
Dan Avram <dan.avram@mta.ro>
2020-10-21 13:28:25 +0300

    Adaugare clasa Utilizator
    Signed-off-by: Dan Avram <dan.avram@mta.ro>

---
+++ src/ro/mta/facc/selab/Utilizator.java
@@ -0,0 +1,11 @@
+package ro.mta.facc.selab;
+
+import java.util.ArrayList;
+
+public class Utilizator {
+    String nume;
+    ArrayList<Carte> carti;
+
+    public void imprumuta_carte(Carte c)
+    {}
+}
